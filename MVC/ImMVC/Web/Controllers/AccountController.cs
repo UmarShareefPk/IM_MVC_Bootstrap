85ca5497daf61b58bf69bb8ece7dcd4456a738e4
Umar Shareef <Omar.Sharif91@gmail.com>
2021-04-06 02:12:32 +0500

    MVC added

---
+++ MVC/ImMVC/Web/Controllers/AccountController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string username , string password)
+        {
+            return Redirect("/Incident/IncidentListing");
+         
+            return View();
+        }
+    }
+}
+
